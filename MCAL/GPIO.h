34e91397c54664ce14c7c750eacff4ebf0815063
omar <eng.omar.amr@gmail.com>
2022-10-02 19:13:50 +0200

    Update: define Periphral regs

--- MCAL/GPIO.h
+++ MCAL/GPIO.h
@@ -38,6 +38,26 @@
 #define PIN6_ID                6
 #define PIN7_ID                7
 
+/******** PortA Registers *******/
+#define PORTA 	(*((volatile uint8 *)0x3B))
+#define DDRA 	(*((volatile uint8 *)0x3A))
+#define PINA 	(*((volatile uint8 *)0x39))
+
+/******** PortB Registers *******/
+#define PORTB 	(*((volatile uint8 *)0x38))
+#define DDRB 	(*((volatile uint8 *)0x37))
+#define PINB 	(*((volatile uint8 *)0x36))
+
+/******** PortC Registers *******/
+#define PORTC 	(*((volatile uint8 *)0x35))
+#define DDRC 	(*((volatile uint8 *)0x34))
+#define PINC 	(*((volatile uint8 *)0x33))
+
+/******** PortD Registers *******/
+#define PORTD 	(*((volatile uint8 *)0x32))
+#define DDRD 	(*((volatile uint8 *)0x31))
+#define PIND 	(*((volatile uint8 *)0x30))
+
 /*******************************************************************************
  *                              Type Declarations                              *
  *******************************************************************************/
